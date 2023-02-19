bd2916b68c0758a684371dbe79dc7603aa576db2
raindropcity <raindropcity0209@gmail.com>
2023-02-19 22:53:30 +0800

    add router oauth

---
+++ server/routes/modules/oauth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user')
+
+const CLIENT_ID = 'caa7bc3d870a03fdc550'
+const CLIENT_SECRET = 'aee41db363ef72e62a58b32651bb8c6c1d43b78a'
+
+
+
+
+module.exports = router
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,9 +3,12 @@ const express = require('express')
 const router = express.Router()
 const todo = require('./modules/todo').router
 const home = require('./modules/home')
+const oauth = require('./modules/oauth')
 
 router.use(home)
 
 router.use(todo)
+
+router.use(oauth)
 
 module.exports = router
