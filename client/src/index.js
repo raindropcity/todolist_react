7c12e5e572c660fedb64f1e47b02f34885281182
raindropcity <raindropcity0209@gmail.com>
2023-02-19 21:19:28 +0800

    remove React's strict mode for Github OAuth

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // 為了串接Github的OAuth，移除strict mode ，因為strict mode會render二次，將造成Github第一次給的token失效
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
